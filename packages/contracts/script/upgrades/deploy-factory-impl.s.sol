289bc03f3f588ce7d1320f6ded086b8706e9ec73
Pilou <76021631+0xPilou@users.noreply.github.com>
2025-11-13 13:51:05 +0100

    fix: revisit deploy script

--- packages/contracts/script/upgrades/deploy-factory-impl.s.sol
+++ packages/contracts/script/upgrades/deploy-factory-impl.s.sol
@@ -7,9 +7,9 @@ import { FluidLockerFactory } from "src/FluidLockerFactory.sol";
 import { IStakingRewardController } from "src/interfaces/IStakingRewardController.sol";
 
 /*
-LOCKER_BEACON_ADDRESS=0xf2880c6D68080393C1784f978417a96ab4f37c38 \
-STAKING_REWARD_CONTROLLER_ADDRESS=0x9FC0Bb109F3e733Bd84B30F8D89685b0304fC018 \
-forge script script/upgrades/deploy-factory-impl.s.sol:DeployFluidLockerFactoyrImplementation --ffi --rpc-url $BASE_SEPOLIA_RPC_URL --broadcast --verify -vvv --etherscan-api-key $BASESCAN_API_KEY
+LOCKER_BEACON_ADDRESS=0x664161f0974F5B17FB1fD3FDcE5D1679E829176c \
+STAKING_REWARD_CONTROLLER_ADDRESS=0xb19Ae25A98d352B36CED60F93db926247535048b \
+forge script script/upgrades/deploy-factory-impl.s.sol:DeployFluidLockerFactoyrImplementation --ffi --account SUP_DEPLOYER --rpc-url $BASE_MAINNET_RPC_URL --broadcast --verify -vvv --etherscan-api-key $ETHERSCAN_API_KEY
 */
 contract DeployFluidLockerFactoyrImplementation is Script {
     function setUp() public { }
@@ -20,14 +20,7 @@ contract DeployFluidLockerFactoyrImplementation is Script {
         address lockerBeaconAddress = vm.envAddress("LOCKER_BEACON_ADDRESS");
         address stakingRewardControllerAddress = vm.envAddress("STAKING_REWARD_CONTROLLER_ADDRESS");
 
-        // Deployer settings
-        uint256 deployerPrivKey = vm.envOr("PRIVATE_KEY", uint256(0));
-
-        if (deployerPrivKey != 0) {
-            vm.startBroadcast(deployerPrivKey);
-        } else {
-            vm.startBroadcast();
-        }
+        vm.startBroadcast();
 
         console2.log("LOCKER_BEACON_ADDRESS=%s", lockerBeaconAddress);
         console2.log("STAKING_REWARD_CONTROLLER_ADDRESS %s", stakingRewardControllerAddress);
@@ -35,6 +28,8 @@ contract DeployFluidLockerFactoyrImplementation is Script {
         FluidLockerFactory fluidLockerFactory =
             new FluidLockerFactory(lockerBeaconAddress, IStakingRewardController(stakingRewardControllerAddress), false);
         console2.log("FluidLockerFactory implementation deployed at: ", address(fluidLockerFactory));
+
+        vm.stopBroadcast();
     }
 
     function _showGitRevision() internal {
