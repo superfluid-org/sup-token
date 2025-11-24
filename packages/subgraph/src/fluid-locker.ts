f33789dc06f2c220c5f58896ae4e60aaa6444a81
elvijsTDL <dzirkals@gmail.com>
2025-11-24 12:36:54 +0200

    Add instant withdraws and fontaines to the locker entity

--- packages/subgraph/src/fluid-locker.ts
+++ packages/subgraph/src/fluid-locker.ts
@@ -1,9 +1,17 @@
-import { BigInt } from "@graphprotocol/graph-ts";
-import { FluidStreamClaimEvent, ClaimEventUnit } from "../generated/schema";
+import { Address, BigInt } from "@graphprotocol/graph-ts";
+import {
+  FluidStreamClaimEvent,
+  ClaimEventUnit,
+  Fontaine,
+  InstantUnlock,
+} from "../generated/schema";
 import {
   FluidStreamClaimed as FluidStreamClaimedEvent,
   FluidStreamsClaimed as FluidStreamsClaimedEvent,
+  FluidUnlocked as FluidUnlockedEvent,
 } from "../generated/templates/FluidLocker/FluidLocker";
+
+const ZERO_ADDRESS = Address.zero();
 
 export function handleFluidStreamClaimed(event: FluidStreamClaimedEvent): void {
   const streamClaimEvent = new FluidStreamClaimEvent(
@@ -44,5 +52,30 @@ export function handleFluidStreamClaimedBulk(event: FluidStreamsClaimedEvent): v
     claimUnit.programId = event.params.programIds[i].toString();
     claimUnit.amount = BigInt.fromU32(event.params.totalProgramUnits[i]);
     claimUnit.save();
+  }
+}
+
+export function handleFluidUnlocked(event: FluidUnlockedEvent): void {
+  if (event.params.fontaine.notEqual(ZERO_ADDRESS)) {
+    let fontaine = new Fontaine(event.params.fontaine);
+    fontaine.locker = event.address;
+    fontaine.amountUnlocked = event.params.availableBalance;
+    fontaine.unlockPeriod = event.params.unlockPeriod;
+    fontaine.recipient = event.params.recipient;
+    fontaine.blockNumber = event.block.number;
+    fontaine.blockTimestamp = event.block.timestamp;
+    fontaine.transactionHash = event.transaction.hash;
+    fontaine.save();
+  } else {
+    let instantUnlock = new InstantUnlock(
+      event.transaction.hash.concatI32(event.logIndex.toI32())
+    );
+    instantUnlock.locker = event.address;
+    instantUnlock.amountUnlocked = event.params.availableBalance;
+    instantUnlock.recipient = event.params.recipient;
+    instantUnlock.blockNumber = event.block.number;
+    instantUnlock.blockTimestamp = event.block.timestamp;
+    instantUnlock.transactionHash = event.transaction.hash;
+    instantUnlock.save();
   }
 }
